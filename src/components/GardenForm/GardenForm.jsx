9462a9ae5cb1f60df28d8021984a07f9dd727f19
William Mayo <wlmayo@gmail.com>
2016-11-22 11:38:40 -0500

    initial commit

---
+++ src/components/GardenForm/GardenForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const GardenForm = (props)=> {
+    return (
+
+    <div>
+    <h3>Create A Garden</h3>
+      <form>
+        <input type="text"/>
+        <button>Create!</button>
+      </form>
+
+    </div>
+
+    );
+}
+
+export default GardenForm;
